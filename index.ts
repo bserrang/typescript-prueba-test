4eac88b81d0340d47675a790e7e19c83eaf933cc
bserrang <bserrang@everis.com>
2021-01-13 16:43:22 +0100

    1 file modified

--- index.ts
+++ index.ts
@@ -1,23 +1,39 @@
 // Import stylesheets
-import './style.css';
+import "./style.css";
 
 // Write TypeScript code!
-const appDiv: HTMLElement = document.getElementById('app');
+const appDiv: HTMLElement = document.getElementById("app");
 appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
 
-class Pru{
-  x:string;
+class Pru {
+  x: string;
   public static y = 5;
 
-  constructor(x:string){
+  constructor(x: string) {
     this.x = x;
   }
-  show(){
+  show() {
+    console.log(this.x);
+  }
+}
+class Pru2 {
+  x: string;
+  public static y = 5;
+
+  constructor(x: string) {
+    this.x = x;
+  }
+  show2() {
     console.log(this.x);
   }
 }
 
-let a = new Pru('dddd');
+let a = new Pru("dddd");
 a.show();
 Pru.y = 30;
 console.log(Pru.y);
+
+function isPru(instancia: Pru | Pru2): instancia is Pru {
+  return (instancia as Pru).show !== undefined;
+}
+console.log(isPru(a));
