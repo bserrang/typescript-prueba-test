6a20931f2273adfc8a9615a13eccf88e374a72b7
bserrang <bserrang@everis.com>
2021-01-13 16:22:25 +0100

    1 file modified

--- index.ts
+++ index.ts
@@ -4,3 +4,20 @@ import './style.css';
 // Write TypeScript code!
 const appDiv: HTMLElement = document.getElementById('app');
 appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+
+class Pru{
+  x:string;
+  public static y = 5;
+
+  constructor(x:string){
+    this.x = x;
+  }
+  show(){
+    console.log(this.x);
+  }
+}
+
+let a = new Pru('dddd');
+a.show();
+Pru.y = 30;
+console.log(Pru.y);
